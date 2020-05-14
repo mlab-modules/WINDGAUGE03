63df87c7bae1fa6c3ba55a69c43dbc76fedf1949
Jan Kott <jan.kott.88@gmail.com>
2020-05-14 14:24:27 +0200

    MLAB-project/Modules#12 Updated get_cog.py script. Still work-in-progress

--- cad/get_cog.py
+++ cad/get_cog.py
@@ -2,69 +2,33 @@
 
 import math
 import numpy as np
-#import gcodeParser
 import xml.etree.ElementTree as ET
 import subprocess
 
 # I used this function to recalculate YAGV results.
 # Used here to make script work without YAGV.
 def get_gcode_cog_2(filename):
-    last_coord = {'X':0, 'Y':0, 'Z':0, 'E':0, 'F':0}
-    new_coord = {'X':0, 'Y':0, 'Z':0, 'E':0, 'F':0}
-    cog_list_x = []
-    cog_list_y = []
-    cog_list_z = []
-    mass_list = []
     parameters = {}
     with open(filename, 'r') as fp:
         line = fp.readline()
         while line:
-            # I see only those options (usually in this order):
-            # 1) Move vertically - G1 Z0.300 F7800.000
-            # 2) Retract - G1 E-2.00000 F2400.00000
-            # 3) Reset thread coordinates - G92 E0
-            # 4) Move horizontally without printing - G1 X70.696 Y70.696 F7800.000
-            # 5) Push retracted thread back in - G1 E2.00000 F2400.00000
-            # 6) Set feed rate - G1 F1800.000
-            # 7+) Move horizontally and print - G1 X79.007 Y78.605 E10.75293
             cmd = line.split()
             if not cmd:
                 line = fp.readline()
                 continue
-            if cmd[0] == 'G1':
-                if cmd[1][0] == 'F': # Set feed rate
-                    pass
-                else:
-                    for arg in cmd[1:]:
-                        new_coord[arg[0]] = float(arg[1:])
-                if cmd[1][0] == 'Z': # Move vertically
-                    pass
-                elif cmd[1][0] == 'E': # Retract or push back
-                    pass
-                elif 'E' in '|'.join(cmd[1:]): # Print
-                    cogx = (last_coord['X'] + new_coord['X'])/2
-                    cogy = (last_coord['Y'] + new_coord['Y'])/2
-                    mass = (new_coord['E'] - last_coord['E'])
-                    cog_list_x.append(cogx)
-                    cog_list_y.append(cogy)
-                    cog_list_z.append(new_coord['Z'])
-                    mass_list.append(mass)
-                last_coord = dict(new_coord)
-            elif cmd[0] == 'G92':
-                # This is command G92 used just to reset thread coordinate ("E")
-                last_coord['E'] = 0
-            elif cmd[0] == ';' and cmd[2] == '=' and len(cmd) == 4:
-                parameters[cmd[1]] = cmd[3]
+            if cmd[0] == ';' and '=' in cmd:
+                param_name = ' '.join(cmd[1:cmd.index('=')])
+                param_value = ' '.join(cmd[cmd.index('=') + 1:])
+                parameters[param_name] = param_value
             line = fp.readline()
-    cgx = np.sum(np.multiply(cog_list_x, mass_list))/np.sum(mass_list)
-    cgy = np.sum(np.multiply(cog_list_y, mass_list))/np.sum(mass_list)
-    cgz = np.sum(np.multiply(cog_list_z, mass_list))/np.sum(mass_list)
+    cgx = float(parameters['cog_x'])
+    cgy = float(parameters['cog_y'])
+    cgz = float(parameters['cog_z'])
+    mass = float(parameters['filament used'].strip('g'))
     x_move = int(parameters['new_offset_X']) - int(parameters['original_offset_X'])
     y_move = int(parameters['new_offset_Y']) - int(parameters['original_offset_Y'])
     z_move = int(parameters['new_offset_Z']) - int(parameters['original_offset_Z'])
-    mass_total = np.sum(mass_list) * 3.14 * (float(parameters['filament_diameter'])/2) ** 2 * float(parameters['filament_density'])
-
-    return cgx - x_move, cgy - y_move, cgz - z_move, mass_total
+    return cgx - x_move, cgy - y_move, cgz - z_move, mass
 
 def bisect(lst, current_index, direction):
     if direction == "UP":
@@ -133,17 +97,6 @@ def rotate_3d(point, rotations):
     x, y = rotate((x, y), math.radians(r3))
     return x, y, z
 
-#def get_gcode_cog(filename):
-#    """
-#    Get COG and MASS from gcode.
-#
-#    Filename should be provided with path to file.
-#    """
-#    parser = gcodeParser.GcodeParser()
-#    model = parser.parseFile(filename)
-#    i = model.getMetadata()
-#    return i['cog']['model']['X'], i['cog']['model']['Y'], i['cog']['model']['Z'], i['material']['mass']
-
 def get_component_cog(rotations, movements, filename):
     mx, my, mz = movements
     x, y, z, m = get_gcode_cog_2(filename)
@@ -156,14 +109,15 @@ zc = [] # z of components
 mc = [] # m of components
 
 #TODO read components from assembly?
+# Columns are path to gcode | rotations | movements | mass in grams
 components = [
     ["../amf/gcode/WINDGAUGE_R03_mod.gcode", [270,  0,   0], [    0,    -84,   198], 0],
     ["../amf/gcode/WINDGAUGE_R04.gcode"    , [  0,  0,   0], [    0,     48,   219], 0],
     ["../amf/gcode/WINDGAUGE_R05.gcode"    , [ 90,  0, 270], [    0, -111.7,   198], 0],
-    ["../amf/gcode/BOLT_M3x12.gcode"       , [0  , 0,   90], [-13.6,   45.5,   216], 1000],
-    ["../amf/gcode/BOLT_M3x12.gcode"       , [0  , 0,   90], [ 13.6,   45.5,   216], 1000],
-    ["../amf/gcode/BOLT_M3x12.gcode"       , [0  , 90,   0], [-4.65,  -77.7, 222.3], 1000],
-    ["../amf/gcode/BOLT_M3x12.gcode"       , [0  , 90,   0], [-4.65,  -77.7, 173.7], 1000],
+    ["../amf/gcode/BOLT_M3x12.gcode"       , [0  , 0,   90], [-13.6,   45.5,   216], 1],
+    ["../amf/gcode/BOLT_M3x12.gcode"       , [0  , 0,   90], [ 13.6,   45.5,   216], 1],
+    ["../amf/gcode/BOLT_M3x12.gcode"       , [0  , 90,   0], [-4.65,  -77.7, 222.3], 1],
+    ["../amf/gcode/BOLT_M3x12.gcode"       , [0  , 90,   0], [-4.65,  -77.7, 173.7], 1],
 ]
 
 print("########################## START ##################################################")
@@ -173,7 +127,7 @@ for c in components:
   x, y, z, m = get_component_cog(c[1], c[2], c[0])
   if c[3] > 0:
     m = c[3]
-  print(c[0], round(x, 2), round(y, 2), round(z, 2), round(m / 1000, 3))
+  print(c[0], round(x, 2), round(y, 2), round(z, 2), round(m, 3))
 #TODO write back to assembly?
   print("translate([", x,"," , y, ",", z, "])\n\n")
   xc.append(x)
@@ -220,7 +174,7 @@ while abs(mods['x']['cog']) > 0.1 or abs(mods['y']['cog']) > 0.1 or abs(mods['z'
 
 #TODO replace hard coded slicer paths
     print("Slic3r running...")
-    subprocess.run('slic3r.pl --load ../amf/gcode/default.ini --no-gui -j 6 -o result.gcode result.amf > /dev/null', shell=True)
+    subprocess.run('Slic3r --load ../amf/gcode/default.ini --no-gui -j 6 -o result.gcode result.amf > /dev/null', shell=True)
 
     print("Calculating CoG...")
 #TODO replace hard coded input file result.gcode
